6fc567d16f9bad528400b53e6df487a98a22c3cf
Andreas Stöckel <astoecke@uwaterloo.ca>
2019-12-11 16:47:37 -0500

    Assignment 1 done

---
+++ assignments/ipynb_to_pdf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# Create a random folder
+DIR=$( mktemp -d )
+
+# Copy the file we're working on to the temp directory
+cp "$1.ipynb" "$DIR"
+
+# Go to this directory
+cd "$DIR"
+jupyter nbconvert --to latex --template article "$1.ipynb"
+
+# Fetch the student's name
+NAME=$( grep -i "Student Name:" "$1.tex" | sed 's/.*Student Name: \(\\[^ ]\+ \)\?\([^\\}]\+\).*/\2/I' )
+
+# Change a few settings
+sed -i 's/\\maketitle//' "$1.tex"
+sed -i 's/section{/section*{/' "$1.tex"
+sed -i 's/subsection{/subsection*{/' "$1.tex"
+sed -i 's/\(\\documentclass\[\(.*\)\]{article}$\)/\1\n\n    \\usepackage{palatino}\n    \\setlength{\\parindent}{0cm}\n    \\setlength{\\parskip}{1em}\n/' "$1.tex"
+sed -i 's/\\begin{Verbatim}\[\(.*\)\]/\\begin{Verbatim}[\1,fontsize=\\small]/' "$1.tex"
+sed -i 's/\\geometry{.*$/\\geometry{a4paper,top=3cm,bottom=2cm,left=2.5cm,right=2.5cm}\n    \\usepackage{fancyhdr}\n    \\pagestyle{fancy}\n    \\fancyhf{}\n    \\lhead{'"$NAME"'}\n    \\rhead{\\thepage}\n/' "$1.tex"
+
+# Execute xelatex on the file
+xelatex "$1"
+
+# Copy the resulting PDF back and delete the temporary directory
+cd -
+cp "$DIR/$1.pdf" .
+
+# Delete the temporary directory
+rm -rf "$DIR"
